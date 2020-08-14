16b21234b9679e9c8932119a8c8a904249fc523f
kanokphon <o0ogutanlove@gmail.com>
2020-08-14 11:04:27 +0700

    install conmit

--- app/src/main/java/com/example/pong/PongGame.java
+++ app/src/main/java/com/example/pong/PongGame.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
 
 class PongGame extends SurfaceView implements Runnable {
     public PongGame(Context context, int x, int y) {
@@ -18,10 +21,44 @@ class PongGame extends SurfaceView implements Runnable {
         mFontMargin = mScreenX / 75;
         mOurHolder = getHolder();
         mPaint = new Paint();
+        mBall = new Ball(mScreenX);
+        mBall.reset(mScreenX, mScreenY);
+        mBat = new Bat(mScreenX, mScreenY);
 
         startNewGame();
+    }
+
+    @Override
+    public boolean onTouchEvent
+
+            (MotionEvent motionEvent) {
+        switch (motionEvent.getAction() &
+                MotionEvent.ACTION_MASK) {case MotionEvent.ACTION_DOWN:
+        mPaused = false;
+        if
+        (motionEvent.getX() > mScreenX / 2){
+            mBat.setMovementState
+                    (mBat.RIGHT);
+        }
+        else
+        {
+            mBat.setMovementState
+                    (mBat.LEFT);
+        }
+        break;
+        case MotionEvent.ACTION_UP:
+        mBat.setMovementState
+                (mBat.STOPPED);
+        break;
 
     }
+    return true;
+
+    }
+
+
+
+
     public void pause() {
         mPlaying = false;
         try {
@@ -31,6 +68,7 @@ class PongGame extends SurfaceView implements Runnable {
         }
 
     }
+
     public void resume() {
         mPlaying = true;
 // Initialize the instance of Thread
@@ -40,33 +78,36 @@ class PongGame extends SurfaceView implements Runnable {
     }
 
 
-    private void startNewGame(){
+    private void startNewGame() {
         mScore = 0;
         mLives = 3;
     }
+
     private void draw() {
 
-            if (mOurHolder.getSurface().isValid()) {
-                mCanvas = mOurHolder.lockCanvas(); // Lock the canvas (graphics memory)
-                mCanvas.drawColor(Color.argb(255, 26, 128, 182));
-                mPaint.setColor(Color.argb(255, 255, 255, 255));
-                mPaint.setTextSize(mFontSize);
-                mCanvas.drawText("Score: " + mScore + " Lives: " + mLives,
-                        mFontMargin, mFontSize, mPaint);
-                if (DEBUGGING) {
-                    printDebuggingText();
-                }
-                mOurHolder.unlockCanvasAndPost(mCanvas);
+        if (mOurHolder.getSurface().isValid()) {
+            mCanvas = mOurHolder.lockCanvas(); // Lock the canvas (graphics memory)
+            mCanvas.drawColor(Color.argb(255, 26, 128, 182));
+            mPaint.setColor(Color.argb(255, 255, 255, 255));
+            mPaint.setTextSize(mFontSize);
+            mCanvas.drawRect(mBall.getRect(), mPaint);
+            mCanvas.drawRect(mBat.getRect(), mPaint);
+            mCanvas.drawText("Score: " + mScore + " Lives: " + mLives,
+                    mFontMargin, mFontSize, mPaint);
+            if (DEBUGGING) {
+                printDebuggingText();
             }
+            mOurHolder.unlockCanvasAndPost(mCanvas);
+        }
 
     }
 
 
-    private void printDebuggingText(){
+    private void printDebuggingText() {
         int debugSize = mFontSize / 2;
         int debugStart = 150;
         mPaint.setTextSize(debugSize);
-        mCanvas.drawText("FPS: " + mFPS ,
+        mCanvas.drawText("FPS: " + mFPS,
                 10, debugStart + debugSize, mPaint);
     }
 
@@ -93,7 +134,7 @@ class PongGame extends SurfaceView implements Runnable {
     public void run() {
         while (mPlaying) {
             long frameStartTime = System.currentTimeMillis();
-            if(!mPaused){
+            if (!mPaused) {
                 update();
                 detectCollisions();
             }
@@ -103,14 +144,42 @@ class PongGame extends SurfaceView implements Runnable {
                 mFPS = MILLIS_IN_SECOND / timeThisFrame;
             }
 
-            }
-
-
         }
-    private void update() {
+
 
     }
-    private void detectCollisions(){
+
+    private void update() {
+        mBall.update(mFPS);
+        mBat.update(mFPS);
+
+    }
+
+    private void detectCollisions() {
+        if(RectF.intersects(mBat.getRect(),
+                mBall.getRect())) {
+// Realistic-ish bounce
+            mBall.batBounce(mBat.getRect());
+            mBall.increaseVelocity();
+            mScore++;
+        }
+        if(mBall.getRect().bottom > mScreenY){
+            mBall.reverseYVelocity();
+            mLives--;
+            if(mLives == 0){
+                mPaused = true;
+                startNewGame();
+            }
+        }
+        if(mBall.getRect().top < 0){
+            mBall.reverseYVelocity();
+        }
+        if(mBall.getRect().left < 0){
+            mBall.reverseXVelocity();
+        }
+        if(mBall.getRect().right > mScreenX){
+            mBall.reverseXVelocity();
+        }
 
     }
 }
